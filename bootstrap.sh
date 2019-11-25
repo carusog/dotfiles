dbe24237f0b65bcf3d30922a7854e8c64c37d452
Giuseppe Caruso <giuseppe@bonsai-studio.net>
2019-11-25 11:54:54 +0100

    add `.zshrc` to bootstrap file
    At the same time I tried to make the bostrapping script
    unaware of the repo location.
    It will hopefully work… 🐛

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,7 +15,7 @@ function ensure_link {
     # test for symlink THEN test if conf file is in dotfiles THEN create symlink
     # if any of the first two checks will fail, symlink will not be created
     echo "checking or creating ${2}"
-    test -L "$HOME/$2" || test ! -e "$HOME/$1" || ln -s "$HOME/$1" "$HOME/$2"
+    test -L "${HOME}/$2" || test ! -e "${HOME}/$1" || ln -s "${HOME}/$1" "${HOME}/$2"
 }
 
 # vim
@@ -24,12 +24,22 @@ test ! -d ~/.vim || \
 	echo "setting up bkp" && \
 	mkdir -p ~/.dotfiles_bkp && \
 	mv ~/.vim $_ && \
-	ln "$HOME/src/dotfiles/vim" "$HOME/.vim"
+	ln "./vim" "${HOME}/.vim"
 
 echo "Just move DT vim to .vim"
 test -d ~/.vim || \
 	echo "There was no such a directory, so we are creating symlink" && \
-	ln -s "$HOME/src/dotfiles/vim" "$HOME/.vim"
+	ln -s "./vim" "${HOME}/.vim"
 
-ensure_link "src/dotfiles/vim/vimrc" ".vimrc" 
+ensure_link "./vim/vimrc" "${HOME}/.vimrc" 
+
+# zsh
+
+echo "Check for .zshrc existence. Eventually move to bkp folder" 
+test ! -f ${HOME}/.zshrc || \
+	echo "Found a .zshrc file. I am moving it to back up folder" && \
+	mv ${HOME}/.zshrc ${HOME}/.dotfiles_bkp/
+
+ensure_link "./.zshrc" "${HOME}/.zshrc"
+
 
