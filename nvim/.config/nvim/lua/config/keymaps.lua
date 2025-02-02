ddb591c333b56eb8d47bf52c0ccb1b87fd2ac32a
Giuseppe Caruso <peppecaruso@gmail.com>
2025-02-02 18:19:33 +0100

    add copilot toggle command

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,28 @@ vim.keymap.set("i", "kj", "<Esc>", { desc = "esc from insert mode" })
 vim.keymap.set("i", "jj", "<Esc> :wa<CR>", { desc = "esc from insert mode and save all files", silent = true })
 
 -- Terminal
+
+local Snacks = require("snacks")
+local copilot_exists = pcall(require, "copilot")
+
+-- Toggle Copilot
+-- See: https://github.com/LazyVim/LazyVim/discussions/4232#discussioncomment-11191278
+if copilot_exists then
+  Snacks.toggle({
+    name = "Copilot Completion",
+    color = {
+      enabled = "azure",
+      disabled = "orange",
+    },
+    get = function()
+      return not require("copilot.client").is_disabled()
+    end,
+    set = function(state)
+      if state then
+        require("copilot.command").enable()
+      else
+        require("copilot.command").disable()
+      end
+    end,
+  }):map("<leader>at")
+end
