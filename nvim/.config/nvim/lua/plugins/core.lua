543311587216e0bb45924209c2faf28f2e3526ca
Giuseppe Caruso <peppecaruso@gmail.com>
2025-02-02 18:19:47 +0100

    add prisma support

--- nvim/.config/nvim/lua/plugins/core.lua
+++ nvim/.config/nvim/lua/plugins/core.lua
@@ -6,4 +6,18 @@ return {
       colorscheme = "catppuccin",
     },
   },
+  -- add symbols-outline (example plugin)
+  {
+    -- "simrat39/symbols-outline.nvim",
+    -- cmd = "SymbolsOutline",
+    -- keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    -- opts = {
+    --   -- add your options that should be passed to the setup() function here
+    --   position = "right",
+    -- },
+  },
+  -- add syntax highlight to .prisma files
+  {
+    "prisma/vim-prisma",
+  },
 }
